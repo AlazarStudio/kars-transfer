421795a9ebc2243ceb8be13e98c97b1389593beb
Kubanov Gisa <kubanovgisa423@gmail.com>
2025-11-08 23:12:26 +0300

    Hot Fix

--- exports/dateTimeFormatterVersion2.js
+++ exports/dateTimeFormatterVersion2.js
@@ -17,4 +17,18 @@ export const dateFormatter =(inputDate) => {
     }
     
     return `${moscowDate["yearMonthDate"]}T${moscowDate["hour"]}:${moscowDate["minute"]}:${moscowDate["second"]}`
+
+
+
+
+    //  VERSION 3
+
+    // const strLocalInputDate = inputDate.toLocaleString()      // DD.MM.YYYY, HH:MM:SS
+
+    // const yearMonthDayLst = strLocalInputDate.split(', ')[0].split('.')   //  DD.MM.YYYY, HH:MM:SS   -->   ["DD.MM.YYYY", "HH:MM:SS"]   -->   "DD.MM.YYYY"   -->   ["DD", "MM", "YYYY"] 
+    // const time = strLocalInputDate.split(', ')[1]  // DD.MM.YYYY, HH:MM:SS   -->    ["DD.MM.YYYY", "HH:MM:SS"]    -->    "HH:MM:SS"
+
+    // const yearMonthDay = `${yearMonthDayLst[2]}-${yearMonthDayLst[1]}-${yearMonthDayLst[0]}`    // YYYY-MM-DD
+
+    // return `${yearMonthDay}T${time}`
 }
--- t.js
+++ t.js
@@ -1,3 +1,15 @@
-const s = {key1: 'cheburek'}
+const inputDate = new Date("2025-04-22T18:00:00")
 
-console.log(s['lool'])
+const strInputDate = inputDate.toLocaleString()      // DD.MM.YYYY, HH:MM:SS
+
+const yearMonthDateLst = strInputDate.split(', ')[0].split('.')   //  DD.MM.YYYY, HH:MM:SS   -->   ["DD.MM.YYYY", "HH:MM:SS"]   -->   "DD.MM.YYYY"   -->   ["DD", "MM", "YYYY"] 
+const time = strInputDate.split(', ')[1]  //   HH:MM:SS
+
+const yearMonthDate = `${yearMonthDateLst[2]}-${yearMonthDateLst[1]}-${yearMonthDateLst[0]}`    // YYYY-MM-DD
+
+const result = `${yearMonthDate}T${time}`
+
+console.log(strInputDate)
+console.log(yearMonthDateLst)
+console.log(yearMonthDate)
+console.log(result)
